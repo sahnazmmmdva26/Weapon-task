fe8a31c941b16de749ef4418680ce7c0327768de
FeridQarayev <tu7he6n9s@code.edu.az>
2022-12-13 01:46:34 -0800

    added new file

---
+++ project/project/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            args:
+            try
+            {
+                Console.WriteLine("Capacity:");
+                int capacity = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("bullets:");
+                int bullets = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("discharge:");
+                int discharge = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("mode:");
+                string mode = Console.ReadLine();
+                Weapon weapon = new Weapon(capacity, bullets, discharge, mode);
+
+            again:
+                Console.WriteLine("0 - menu\n1 - Shoot\n2 - Fire\n3 - Get Remain Bullet Count\n4 - Reload \n5 - Change Fire Mode\n6 - Cancel");
+                while (true)
+                {
+                    Console.WriteLine("Choise: ");
+                    int choise = Convert.ToInt16(Console.ReadLine());
+                    switch (choise)
+                    {
+                        case 0:
+                            goto again;
+                        case 1:
+                            weapon.Shoot();
+                            break;
+                        case 2:
+                            weapon.Fire();
+                            break;
+                        case 3:
+                            weapon.GetRemainBulletCount();
+                            break;
+                        case 4:
+                            weapon.Reload();
+                            break;
+                        case 5:
+                            weapon.ChangeFireMode();
+                            break;
+                        case 6:
+                            return;
+                        default:
+                            Console.WriteLine("there isn't option like this");
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong input\n");
+                goto args;
+            }
+                
+
+
+            /*
+              0 - İnformasiya almaq üçün
+              1 - Shoot metodu üçün
+              2 - Fire metodu üçün
+              3 - GetRemainBulletCount metodu üçün
+              4 - Reload metodu üçün
+              5 - ChangeFireMode metodu üçün
+              6 - Proqramdan dayandırmaq üçün
+                   qısayoldur.
+            */
+        }
+    }
+}
---
+++ project/project/Weapon.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Weapon
+    {
+
+        private int _bulletcapacity; //gulle kapasitesi
+        private int _bulletNumbers; //gulle sayi
+        private int _dischargeSecond; // bosalma muddeti
+        private string _shootingMode; //modu
+
+        public int Bulletcapacity
+        {
+            get { return _bulletcapacity; }
+            set 
+            { 
+                if (value>0)
+                {
+
+                _bulletcapacity = value; 
+                }
+                else
+                {
+                    Console.WriteLine("wrong  Bulletcapacity input");
+                    value = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+        }
+
+        public int BulletNumbers
+        {
+            get { return _bulletNumbers; }
+            set
+            {
+                if (value >=0)
+                {
+
+                    _bulletNumbers = value;
+                }
+                else
+                {
+                    Console.WriteLine("wrong BulletNumbers input");
+                    value = Convert.ToInt32(Console.ReadLine());
+
+
+                }
+
+            }
+
+        }
+
+        public int DischargeSecond
+        {
+            get { return _dischargeSecond; }
+            set
+            {
+                if (value > 0)
+                {
+
+                    _dischargeSecond = value;
+                }
+                else
+                {
+                    Console.WriteLine("wrong DischargeSecond input");
+                    value = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+        }
+
+        public string ShootingMode   //təkli və ya avtomatik
+        {
+            get { return _shootingMode; }
+            set 
+            { 
+                if(!string.IsNullOrEmpty(value))
+                {
+                value= value.ToLower().Trim();
+                    if (value == "auto" || value == "single")
+                    {
+                        _shootingMode = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong ShootingMode input");
+                        value= Console.ReadLine();
+                    }
+                }
+                else { Console.WriteLine("wrong ShootingMode input"); }
+                
+            }
+        }
+        public Weapon(int bulletcapacity,int bulletNumbers, int dischargeSecond, string shootingMode)
+        {
+            BulletNumbers= bulletNumbers;
+            DischargeSecond= dischargeSecond;
+            ShootingMode= shootingMode;
+            Bulletcapacity= bulletcapacity;
+        }
+        public void Shoot()
+        {
+            if (BulletNumbers == 0)
+            {
+                Console.WriteLine("empty charger");
+            }
+            else { 
+            BulletNumbers--;
+                Console.WriteLine($"Shooted, number of bullets remaining : {BulletNumbers}"); }
+        }
+        public void Fire()
+        {
+            if (BulletNumbers == 0)
+            {
+                Console.WriteLine("empty charger");
+            }
+            else
+            {
+                int secondforone = DischargeSecond / Bulletcapacity;
+                int result = secondforone * BulletNumbers;
+                BulletNumbers = 0;
+                Console.WriteLine($"Shooted, DischargeSecond : {result}");
+            }
+            
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine($"you need {Bulletcapacity- BulletNumbers} bullet");
+        }
+        public void Reload()
+        {
+            BulletNumbers = Bulletcapacity;
+        }
+      
+        public void ChangeFireMode()
+        {
+            if(_shootingMode=="single")
+            {
+                _shootingMode = "auto";
+            }
+            if (_shootingMode == "auto")
+            {
+                _shootingMode = "single";
+            }
+            Console.WriteLine($"mode: {_shootingMode}");
+        }
+    }
+}
